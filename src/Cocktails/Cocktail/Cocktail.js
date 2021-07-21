f7d5479cde7cb14377bad0e14c48ece02a550ebd
darthinvader <psaradakisj@gmail.com>
2021-07-21 21:05:01 +0300

    [fix] name corrected and null ingredients discarded

--- src/Cocktails/Cocktail/Cocktail.js
+++ src/Cocktails/Cocktail/Cocktail.js
@@ -1,10 +1,12 @@
 import styles from "./Cocktail.module.scss";
+import CocktailDetails from "./CocktailDetails/CocktailDetails";
 const Cocktail = ({ name, img }) => {
   return (
     <>
       <div className={styles.Cocktail}>
         <h1 className={styles.Title}>Your Cocktail Is {name}</h1>
         <img className={styles.Img} src={img} alt="CockTail" />
+        <CocktailDetails name={name} />
       </div>
     </>
   );
--- src/Cocktails/Cocktail/CocktailDetails/CocktailDetails.js
+++ src/Cocktails/Cocktail/CocktailDetails/CocktailDetails.js
@@ -2,20 +2,22 @@ import styles from "./CocktailDetails.module.scss";
 import { useEffect, useState } from "react";
 import cocktailRequest from "../../../requests/cocktailAxios";
 const determineIngredients = (drink) => {
-  const keys = Object.keys(drink).filter((key) =>
-    key.includes("strIngredient")
+  const keys = Object.keys(drink).filter(
+    (key) => key.includes("strIngredient") && drink[key]
   );
   const values = keys.map((key) => drink[key]);
   return values;
 };
 
 const determineMeasurements = (drink) => {
-  const keys = Object.keys(drink).filter((key) => key.includes("strMeasure"));
+  const keys = Object.keys(drink).filter(
+    (key) => key.includes("strMeasure") && drink[key]
+  );
   const values = keys.map((key) => drink[key]);
   return values;
 };
 
-const CocktailDetails = ({ cocktailName }) => {
+const CocktailDetails = ({ name }) => {
   const [ingredients, setIngredients] = useState([]);
   const [instructions, setInstructions] = useState("");
   const [measurements, setMeasurements] = useState([]);
@@ -24,7 +26,8 @@ const CocktailDetails = ({ cocktailName }) => {
   useEffect(() => {
     (async () => {
       try {
-        const response = await cocktailRequest(`search.php?s=${cocktailName}`);
+        const response = await cocktailRequest(`search.php?s=${name}`);
+        console.log(response);
         const [drink] = response.data.drinks;
         setIngredients(determineIngredients(drink));
         setMeasurements(determineMeasurements(drink));
@@ -34,14 +37,13 @@ const CocktailDetails = ({ cocktailName }) => {
         console.error(error);
       }
     })();
-  }, [cocktailName]);
+  }, [name]);
 
   const length = ingredients.length;
   const measuredIngredients = [];
   for (let i = 0; i < length; i++) {
     measuredIngredients.push(
-      <div>
-        <h2>Ingredients:</h2>
+      <div key={ingredients[i]}>
         <p>
           {measurements[i]}
           {ingredients[i]}
@@ -52,6 +54,7 @@ const CocktailDetails = ({ cocktailName }) => {
 
   return (
     <div>
+      <h2>Ingredients:</h2>
       {measuredIngredients}
       <h2>Instructions:</h2>
       <p>{instructions}</p>
