979f9a497c9bb8a7cf97db80b317837f3d5e04c8
darthinvader <psaradakisj@gmail.com>
2021-07-21 19:44:27 +0300

    [feat] request send and cocktail shown

--- src/Cocktails/CocktailWrapper.js
+++ src/Cocktails/CocktailWrapper.js
@@ -5,21 +5,29 @@ import styles from "./CocktailWrapper.module.scss";
 import axios from "axios";
 
 const CocktailWrapper = () => {
-  const [cocktail, setCocktail] = useState({});
+  const [cocktail, setCocktail] = useState(null);
 
   const createCocktail = async (ingredient) => {
-    const response = await axios.get(
-      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
-    );
-    console.log(response);
+    await axios
+      .get(
+        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
+      )
+      .then(({ data }) => {
+        const { drinks } = data;
+        const randomDrink = drinks[Math.floor(Math.random() * drinks.length)];
+        setCocktail({
+          name: randomDrink.strDrink,
+          img: randomDrink.strDrinkThumb,
+        });
+      })
+      .catch((error) => {});
   };
 
-  const CocktailBody =
-    cocktail.length === 0 ? (
-      <Cocktail name={cocktail.name} img={cocktail.img} />
-    ) : (
-      <CocktailForm createCocktail={createCocktail} />
-    );
+  const CocktailBody = cocktail ? (
+    <Cocktail name={cocktail.name} img={cocktail.img} />
+  ) : (
+    <CocktailForm createCocktail={createCocktail} />
+  );
 
   return (
     <>
