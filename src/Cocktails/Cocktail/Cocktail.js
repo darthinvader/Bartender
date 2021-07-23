f4c55522667a37ee12d2ac6627358ba1d91f7e9e
darthinvader <psaradakisj@gmail.com>
2021-07-23 17:11:52 +0300

    [feat] added back Button

--- src/Cocktails/Cocktail/Cocktail.js
+++ src/Cocktails/Cocktail/Cocktail.js
@@ -1,9 +1,13 @@
 import styles from "./Cocktail.module.scss";
 import CocktailDetails from "./CocktailDetails/CocktailDetails";
-const Cocktail = ({ name, img }) => {
+const Cocktail = ({ name, img, returnForm }) => {
   return (
     <>
       <div className={styles.Cocktail}>
+        <p onClick={returnForm} className={styles.ArrowBack}>
+          {"<"}
+        </p>
+
         <h1 className={styles.Title}>Your Cocktail Is {name}</h1>
         <img className={styles.Img} src={img} alt="CockTail" />
         <CocktailDetails name={name} />
